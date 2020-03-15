047f37ef8537a452c7774bd59d5a5cc416448ae5
Marios <mariospatsis@gmail.com>
2020-03-15 10:01:28 +0200

    Delete Favorites

--- components/FavoriteComponent.js
+++ components/FavoriteComponent.js
@@ -4,6 +4,8 @@ import { ListItem } from "react-native-elements";
 import {connect } from "react-redux";
 import {baseUrl} from "../shared/baseUrl";
 import {Loading} from "./LoadingComponent";
+import Swipeout from "react-native-swipeout";
+import { deleteFavorite} from "../redux/ActionCreators";
 
 const mapStateToProps = state =>{
     return {
@@ -11,6 +13,10 @@ const mapStateToProps = state =>{
         favorites: state.favorites
     }
 };
+
+const mapDispatchToProps = dispatch => ({
+    deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId))
+});
 
 class Favorites extends Component {
 
@@ -22,16 +28,26 @@ class Favorites extends Component {
         const { navigate } = this.props.navigation;
 
         const renderMenuItem = ({ item, index}) => {
+            const rightButton = [
+                {
+                    text: 'Delete',
+                    type: 'delete',
+                    onPress: () => this.props.deleteFavorite(item.id)
+                }
+            ];
+
             return (
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    hideChevron={true}
-                    onPress={() => navigate('Dishdetail', {dishId: item.id})}
-                    leftAvatar={{source: {uri: baseUrl + item.image}}}
-                >
-                </ListItem>
+                <Swipeout right={rightButton} autoClose={true}>
+                    <ListItem
+                        key={index}
+                        title={item.name}
+                        subtitle={item.description}
+                        hideChevron={true}
+                        onPress={() => navigate('Dishdetail', {dishId: item.id})}
+                        leftAvatar={{source: {uri: baseUrl + item.image}}}
+                    >
+                    </ListItem>
+                </Swipeout>
             );
         }
 
@@ -58,4 +74,4 @@ class Favorites extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
