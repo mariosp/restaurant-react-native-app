f23ec6e27f406ebc8250193da28e503eed0b3332
Marios <mariospatsis@gmail.com>
2020-03-23 22:55:57 +0200

    React native share api, dish detail share button

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, ScrollView, FlatList, StyleSheet, Modal, Alert, PanResponder} from "react-native";
+import {View, Text, ScrollView, FlatList, StyleSheet, Modal, Alert, PanResponder, Share} from "react-native";
 import {Card, Icon, Button, Rating, Input} from "react-native-elements";
 import {connect } from "react-redux";
 import {baseUrl} from "../shared/baseUrl";
@@ -78,6 +78,16 @@ const RenderDish = (props) =>{
         }
     });
 
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ':' + message + ' ' + url,
+            url: url
+        }, {
+                dialogTitle: "Share" + title
+            });
+    };
+
     if(dish!= null){
         return(
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
@@ -108,6 +118,14 @@ const RenderDish = (props) =>{
                             color="#512DAB"
                             onPress={()=> props.toggleAddCommentModal()}
                         />
+                        <Icon
+                            raised
+                            reverse
+                            name="share"
+                            type="font-awesome"
+                            color="#51D2AB"
+                            onPress={()=> shareDish(dish.name, dish.description, baseUrl+ dish.image)}
+                            />
                     </View>
                 </Card>
             </Animatable.View>
